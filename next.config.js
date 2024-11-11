e6643ba68a985a7a423a1255f350f97a8eeb59fb
Waleed <waleed.mukhtar1999@gmail.com>
2024-11-11 16:15:33 +0500

    added next config

---
+++ next.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+
+    images: {
+        remotePatterns: [
+          {
+            protocol: 'https',
+            hostname: 'media.licdn.com',
+            port: '',
+          },
+        ],
+      },
+  };
+  
