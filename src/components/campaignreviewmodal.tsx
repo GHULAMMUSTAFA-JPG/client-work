51a7703383dbf21e2e19092a3bebe9b5c9f32ff0
Usman <usman.asif@funavry.com>
2024-11-22 16:19:10 +0500

    ui ux

--- src/components/campaignreviewmodal.tsx
+++ src/components/campaignreviewmodal.tsx
@@ -45,7 +45,7 @@ function CampaignReviewModal() {
                                             <div className="tab-content " id="myTabContent">
                                                 <div className="tab-pane fade show active" id="user1-tab-pane" role="tabpanel" aria-labelledby="user1-tab" tabIndex={0}>
                                                     <div className="position-relative mt-3">
-                                                        <input type="email" className="form-control ps-5" id="exampleFormControlInput1" placeholder="Search for Campaigns" />
+                                                        <input type="email" className="form-control ps-5" id="exampleFormControlInput1" placeholder="Search for Creators" />
                                                         <Icon icon="ph:magnifying-glass" width={20} height={20} className='text-secondary position-absolute top-50 start-0 translate-middle-y ms-3' />
                                                         <Icon icon="ph:x" width={20} height={20} className='text-secondary position-absolute top-50 end-0 translate-middle-y me-5 cursor' />
                                                         <Icon icon="akar-icons:settings-vertical" width={20} height={20} className='text-primary position-absolute top-50 end-0 translate-middle-y me-3 cursor' data-bs-toggle="modal" data-bs-target="#exampleModal" />
