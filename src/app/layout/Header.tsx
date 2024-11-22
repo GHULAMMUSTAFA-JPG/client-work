b8841e644f221a62af3edccb65b175f7d38b40df
Usman <usman.asif@funavry.com>
2024-11-22 17:12:10 +0500

    ui ux

--- src/app/layout/Header.tsx
+++ src/app/layout/Header.tsx
@@ -11,7 +11,7 @@ import { useAuth } from "@/contexts/AuthContext";
 export default function Header() {
     const pathname = usePathname(); // Initialize pathname without condition
     const [user, setUser] = useState<any>()
-    const {logout} = useAuth()
+    const { logout } = useAuth()
 
     useEffect(() => {
         setUser(localStorage.getItem("user"))
@@ -24,10 +24,10 @@ export default function Header() {
     return (
         <>
             <header className="navbar-section">
-                <nav className="navbar navbar-expand-lg bg-white">
+                <nav className="navbar bg-white">
                     <div className="container-fluid">
-                        <Icon icon="ic:round-menu" width={24} height={24} />
-                        <button
+                        <Icon icon="ic:round-menu" width={24} height={24} type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasResponsive" aria-controls="offcanvasResponsive" />
+                        {/* <button
                             className="navbar-toggler bg-primary"
                             type="button"
                             data-bs-toggle="collapse"
@@ -37,12 +37,12 @@ export default function Header() {
                             aria-label="Toggle navigation"
                         >
                             <span className="navbar-toggler-icon"></span>
-                        </button>
-                        <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                                {/* Navigation items here */}
-                            </ul>
-                            {/* <form className="d-flex" role="search">
+                        </button> */}
+                        {/* <div className="collapse navbar-collapse" id="navbarSupportedContent"> */}
+                        {/* <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                            Navigation items here
+                        </ul> */}
+                        {/* <form className="d-flex" role="search">
                                 {(pathname !== '/' && pathname !== '/login') && (
                                     <button
                                         className="btn btn-sm btn-outline-primary me-2"
@@ -52,43 +52,43 @@ export default function Header() {
                                         {user?"Sign Out":"Sign In"}
                                     </button>
                                 )} */}
-                            {/* Register button if needed */}
-                            {/* </form> */}
-                            <Icon icon="mingcute:notification-line" width={24} height={24}  className="text-warning" />
-                            <Icon icon="mdi:message-outline" width={21} height={21} className="ms-3 text-warning"/>
-                            <div className="dropdown ms-2">
-                                <a className="btn bg-transparent dropdown-toggle border-0" type="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                    <div className="d-flex align-items-center">
-                                        <Image src="/assets/images/user.jpg" alt="user" width={32} height={32} className="user-img" />
-                                        <p className="mb-0 ms-2">Mark Phillips</p>
-                                        <Icon icon="prime:chevron-down" className="ms-2" width={20} height={20} />
-                                    </div>
+                        {/* Register button if needed */}
+                        {/* </form> */}
+                        <Icon icon="mingcute:notification-line" width={24} height={24} className="text-warning ms-auto" />
+                        <Icon icon="mdi:message-outline" width={21} height={21} className="ms-3 text-warning" />
+                        <div className="dropdown ms-2">
+                            <a className="btn bg-transparent dropdown-toggle border-0" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                <div className="d-flex align-items-center">
+                                    <Image src="/assets/images/user.jpg" alt="user" width={32} height={32} className="user-img" />
+                                    <p className="mb-0 ms-2">Mark Phillips</p>
+                                    <Icon icon="prime:chevron-down" className="ms-2" width={20} height={20} />
+                                </div>
+                            </a>
+
+
+
+                            <ul className="dropdown-menu">
+                                <li><a className="dropdown-item" href="#">
+                                    Update Profile
                                 </a>
-                                 
+                                </li>
+                                <li><a className="dropdown-item" href="#">
+                                    <form className="d-flex" role="search">
+                                        {/* {(pathname !== '/' && pathname !== '/login') && ( */}
+                                        <p
+                                            className="mb-0"
+                                            onClick={navigateToSignIn}
+                                        >
+                                            Sign Out
+                                        </p>
 
-                                 
-                                <ul className="dropdown-menu">
-                                    <li><a className="dropdown-item" href="#">
-                                        Update Profile
-                                    </a>
-                                    </li>
-                                    <li><a className="dropdown-item" href="#">
-                                        <form className="d-flex" role="search">
-                                            {/* {(pathname !== '/' && pathname !== '/login') && ( */}
-                                                <p
-                                                    className="mb-0"
-                                                    onClick={navigateToSignIn}
-                                                >
-                                                    Sign Out
-                                                </p>
-                                            
-                                        </form>
+                                    </form>
 
-                                    </a>
-                                    </li>
-                                </ul>
-                            </div>
+                                </a>
+                                </li>
+                            </ul>
                         </div>
+                        {/* </div> */}
                     </div>
                 </nav>
             </header>
--- src/app/layout/Sidebar.tsx
+++ src/app/layout/Sidebar.tsx
@@ -13,33 +13,40 @@ const Sidebar: React.FC<SidebarProps> = ({ menuItems }) => {
   const [isActive, setIsActive] = useState<number>(0)
 
   return (
-    <div className={`d-flex flex-column sidebar`}>
-      <a className="navbar-brand" href="#">
-        <Image
-          src="/assets/images/logo.png"
-          alt="logo"
-          width={100}
-          height={30}
-          className="img-fluid"
-        />
-      </a>
-      <ul className="nav flex-column">
-        {menuItems.map((item, index) => {
-          return (
-            <li key={index} className="nav-item">
-              <Link
-                onClick={() => { setIsActive(index) }}
-                href={item.href}
-                className={`nav-link d-flex align-items-center ${isActive == index ? 'active' : ''}`}
-              >
-                {item.icon && <i className={`${item.icon} me-3`}></i>}
-                {item.label}
-              </Link>
-            </li>
-          );
-        })}
-      </ul>
-    </div>
+    <>
+      <div className="offcanvas-lg offcanvas-start" tabIndex={-1} id="offcanvasResponsive" aria-labelledby="offcanvasResponsiveLabel">
+        <div className="offcanvas-body">
+          <button type="button" className="btn-close text-white float-end d-lg-none" data-bs-dismiss="offcanvas" data-bs-target="#offcanvasResponsive" aria-label="Close"></button>
+          <div className={`d-flex flex-column sidebar`}>
+            <a className="navbar-brand" href="#">
+              <Image
+                src="/assets/images/logo.png"
+                alt="logo"
+                width={100}
+                height={30}
+                className="img-fluid"
+              />
+            </a>
+            <ul className="nav flex-column">
+              {menuItems.map((item, index) => {
+                return (
+                  <li key={index} className="nav-item">
+                    <Link
+                      onClick={() => { setIsActive(index) }}
+                      href={item.href}
+                      className={`nav-link d-flex align-items-center ${isActive == index ? 'active' : ''}`}
+                    >
+                      {item.icon && <i className={`${item.icon} me-3`}></i>}
+                      {item.label}
+                    </Link>
+                  </li>
+                );
+              })}
+            </ul>
+          </div>
+        </div>
+      </div>
+    </>
   );
 };
 
