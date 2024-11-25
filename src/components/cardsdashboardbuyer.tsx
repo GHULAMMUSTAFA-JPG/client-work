6e905745d33e203c722f5749ebc7c2df83377be0
Usman <usman.asif@funavry.com>
2024-11-25 17:17:52 +0500

    ui ux

--- src/components/cardsdashboardbuyer.tsx
+++ src/components/cardsdashboardbuyer.tsx
@@ -22,15 +22,15 @@ function CardsDashboardBuyer() {
                             </div>
                             <div className='flex-grow-1'>
                                 <div className='d-flex justify-content-between align-items-center'>
-                                    <h5 className='mb-0'>Frontend Developer, Webflow, Wordpress, Shopify, Email Templates</h5>
-                                    <div className='d-flex gap-2 align-items-start'>
+                                    <h5 className='mb-0'>Mark Ellis</h5>
+                                    {/* <div className='d-flex gap-2 align-items-start'>
                                         <button type="button" className="btn btn-outline-primary btn-sm px-4 fs-12 btn-sm nowrap">
                                             Hire
                                         </button>
                                         <button type="button" className="btn btn-primary btn-sm px-4 fs-12 btn-sm nowrap">
                                             Invite
                                         </button>
-                                    </div>
+                                    </div> */}
                                 </div>
                                 <p className='mb-1'>Newyork, America</p>
                                 <div className='d-flex align-items-center mb-2'>
@@ -44,22 +44,6 @@ function CardsDashboardBuyer() {
                                 <div className='d-flex align-items-center mb-3'>
                                     <p className='mb-0 text-secondary'>has <span className='text-dark fw-medium'>13 relevant skills</span> to your job</p>
                                 </div>
-                                <div className='d-flex gap-2 flex-wrap mb-2'>
-                                    <span className="badge bg-success text-secondary rounded-pill fw-light border border-transparent">Backend developer</span>
-                                    <span className="badge bg-success text-secondary rounded-pill fw-light border border-transparent">Front end developer</span>
-                                    <span className="badge bg-success text-secondary rounded-pill fw-light border border-transparent">IT officer</span>
-                                    <span className="badge bg-success text-secondary rounded-pill fw-light border border-transparent">Adobe Premire pro</span>
-                                    <span className="badge bg-success text-secondary rounded-pill fw-light border border-transparent">Software</span>
-                                    <span className="badge bg-success text-secondary rounded-pill fw-light border border-transparent">Adone After Effets</span>
-                                    <span className="badge bg-success text-secondary rounded-pill fw-light border border-transparent">Content Writing</span>
-                                    <span className="badge bg-success text-secondary rounded-pill fw-light border border-transparent">Bootstrap</span>
-                                    <span className="badge bg-success text-secondary rounded-pill fw-light border border-transparent">SCSS</span>
-                                </div>
-                                {/* <div className='d-flex align-items-center'>
-                                                                    <Icon icon="majesticons:settings-cog-check" className='text-primary me-1' />
-                                                                    <p className='fs-9 mb-0 text-warning me-2'>payment verified</p>
-                                                                    <p className='fs-9 mb-0'>$10k+ <span className='text-warning'>spent</span></p>
-                                                                </div> */}
                             </div>
                         </div>
                     </div>
