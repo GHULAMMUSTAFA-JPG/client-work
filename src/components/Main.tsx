865872fecdee7739a265b9585c758c9e9f844b0e
Waleed <waleed.mukhtar1999@gmail.com>
2024-11-25 16:52:26 +0500

    routing defined

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -7,42 +7,46 @@ import Footer from "@/app/layout/Footer"
 import Sidebar from '@/app/layout/Sidebar';
 import withAuth from '@/utils/withAuth';
 
-const menuItems = [
-  // Creators dashbaord menus
-  { label: "Dashboard", href: "/homepage", icon: "bi bi-house" },
-  { label: "Campaigns", href: "/campaigns", icon: "bi bi-briefcase" },
-  // { label: "Discover", href: "/discover", icon: "bi bi-kanban" },
-  { label: "My Company", href: "/creatordashboard", icon: "bi bi-building" },
-  { label: "Analytics", href: "/analytics", icon: "bi bi-bar-chart" },
-
-  // Buyers dashbaord menus
-  { label: "Dashboard", href: "/homepagebuyer", icon: "bi bi-briefcase" },
-  { label: "Campaigns", href: "/buyerdashboard", icon: "bi bi-briefcase" },
-  { label: "My Company", href: "/mycreatorsbuyer", icon: "bi bi-briefcase" },
-  { label: "Analytics", href: "/analyticsbuyer", icon: "bi bi-briefcase" },
-  //   { label: "Logout", href: "/settings", icon: "bi bi-gear" },
-
-];
 
 const Main = ({
-  children, isAuthenticated
+  children, isAuthenticated, user
 }: any) => {
-  return (
-    <>
-      {isAuthenticated ? (
-        <div className="d-flex">
-          <Sidebar menuItems={menuItems} />
-          <div className="flex-grow-1">
-            <Header />
-            <main className='main-content'>{children}</main>
-            <Footer />
+
+
+  const creatorMenuItems = [
+    { label: "Dashboard", href: "/homepage", icon: "bi bi-house" },
+    { label: "Campaigns", href: "/campaigns", icon: "bi bi-briefcase" },
+    { label: "My Company", href: "/creatordashboard", icon: "bi bi-building" },
+    { label: "Analytics", href: "/analytics", icon: "bi bi-bar-chart" },
+  ];
+
+  const buyerMenuItems = [
+    { label: "Dashboard", href: "/homepagebuyer", icon: "bi bi-house" },
+    { label: "Campaigns", href: "/buyerdashboard", icon: "bi bi-briefcase" },
+    { label: "My Company", href: "/mycreatorsbuyer", icon: "bi bi-building" },
+    { label: "Analytics", href: "/analyticsbuyer", icon: "bi bi-bar-chart" },
+  ];
+
+  // Determine which menu items to show based on role
+  const menuItems =
+    user?.role === "creator" ? creatorMenuItems : buyerMenuItems;
+
+    return (
+      <>
+        {isAuthenticated ? (
+          <div className="d-flex">
+            <Sidebar menuItems={menuItems} />
+            <div className="flex-grow-1">
+              <Header />
+              <main className="main-content">{children}</main>
+              <Footer />
+            </div>
           </div>
-        </div>
-      ) : (
-        <main>{children}</main>
-      )}
-    </>
-  );
-};
+        ) : (
+          <main>{children}</main>
+        )}
+      </>
+    );
+  };
 
 export default withAuth(Main);
