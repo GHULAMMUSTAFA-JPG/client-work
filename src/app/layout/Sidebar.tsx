125db920db2612248a0d244342505b1e32d32454
Usman <usman.asif@funavry.com>
2024-12-26 17:43:12 +0500

    ui ux

--- src/app/layout/Sidebar.tsx
+++ src/app/layout/Sidebar.tsx
@@ -10,38 +10,46 @@ interface SidebarProps {
 }
 
 const Sidebar: React.FC<SidebarProps> = ({ menuItems }) => {
-  const [isActive, setIsActive] = useState<number>(0)
+  const [isActive, setIsActive] = useState<number>(0);
+  const [isExpanded, setIsExpanded] = useState(false);
 
   return (
     <>
-      <div className="offcanvas-lg offcanvas-start" tabIndex={-1} id="offcanvasResponsive" aria-labelledby="offcanvasResponsiveLabel">
+      <div 
+        className="offcanvas-lg offcanvas-start" 
+        tabIndex={-1} 
+        id="offcanvasResponsive" 
+        aria-labelledby="offcanvasResponsiveLabel"
+      >
         <div className="offcanvas-body">
           <button type="button" className="btn-close text-white float-end d-lg-none" data-bs-dismiss="offcanvas" data-bs-target="#offcanvasResponsive" aria-label="Close"></button>
-          <div className={`sidebar`}>
+          <div 
+            className={`sidebar ${isExpanded ? 'expanded' : ''}`}
+            onMouseEnter={() => setIsExpanded(true)}
+            onMouseLeave={() => setIsExpanded(false)}
+          >
             <a className="navbar-brand" href="#">
               <Image
                 src="/assets/images/synnc-logo.svg"
                 alt="logo"
                 width={100}
                 height={30}
-                className="img-fluid p-3"
+                className="img-fluid p-3 ps-0"
               />
             </a>
             <ul className="nav">
-              {menuItems.map((item, index) => {
-                return (
-                  <li key={index} className="nav-item">
-                    <Link
-                      onClick={() => { setIsActive(index) }}
-                      href={item.href}
-                      className={`nav-link d-flex align-items-center ${isActive == index ? 'active' : ''}`}
-                    >
-                      {item.icon && <i className={`${item.icon} me-2`}></i>}
-                      {item.label}
-                    </Link>
-                  </li>
-                );
-              })}
+              {menuItems.map((item, index) => (
+                <li key={index} className="nav-item">
+                  <Link
+                    onClick={() => { setIsActive(index) }}
+                    href={item.href}
+                    className={`nav-link d-flex align-items-center ${isActive == index ? 'active' : ''}`}
+                  >
+                    {item.icon && <i className={`${item.icon}`}></i>}
+                    {isExpanded && <span className="nav-label">{item.label}</span>}
+                  </Link>
+                </li>
+              ))}
             </ul>
           </div>
         </div>
