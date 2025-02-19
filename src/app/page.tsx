60a52b312e788730991f6c71385d280a19c1e43c
waleedmukhtar1 <waleed.mukhtar1999@gmail.com>
2025-02-19 15:55:15 +0500

    if loggedin then /route updated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,34 @@
-// import Image from "next/image";
-// import styles from "./page.module.css";
-
-import Homepage from "./homepage/page";
+"use client";
+import { useEffect, useState } from "react";
+import { useRouter } from "next/navigation";
 import Login from "./login/page";
 
-
 export default function Mainpage() {
-  return (
-    <Login />
-    // <></>
-  );
+  const router = useRouter();
+  const [isLoading, setIsLoading] = useState(true); // Track loading state
+  useEffect(() => {
+    requestAnimationFrame(() => {
+      const storedUserData = localStorage.getItem("user");
+      const storeduser = storedUserData ? JSON.parse(storedUserData) : null;
+      console.log("storeduser", storeduser);
+      if (storeduser) {
+        if (
+          storeduser.isBuyer &&
+          storeduser.subscription_status.has_active_subscription == true
+        ) {
+          router.push("/homepagebuyer");
+        } else {
+          router.push("/homepage");
+        }
+      }
+    });
+  }, []);
+
+  // During loading, render nothing or a loading indicator
+  if (isLoading) {
+    return null; // Or return a loading spinner, if preferred
+  }
+
+  // If no user data or redirection has occurred, render the Login component
+  return <Login />;
 }
