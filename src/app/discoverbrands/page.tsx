f70d9385487952584f333dc873ece003c0c7454e
waleedmukhtar1 <waleed.mukhtar1999@gmail.com>
2025-02-20 17:53:22 +0500

    added new page

---
+++ src/app/discoverbrands/page.tsx
@@ -0,0 +1,17 @@
+// "use client"
+// import React, { useEffect, useState } from 'react'
+
+// export default function page() {
+
+// const [brands, setbrands] = useState([]);
+// const [brands, setbrands] = useState([]);
+
+//     useEffect(() => {
+//       first
+
+//     }, [third])
+
+//   return (
+//     <div>page</div>
+//   )
+// }
